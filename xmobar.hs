503a3b0224325f1ed96901c3f4c950f472b260f1
Isaak Lim <isaak.lim@rwth-aachen.de>
2015-11-27 09:57:44 +0100

    fixed xmobar positioning

--- xmobar.hs
+++ xmobar.hs
@@ -7,10 +7,11 @@ Config {
     font = "xft:Fixed-8",
     bgColor = "#000000",
     fgColor = "#ffffff",
-    position = Static { xpos = 0, ypos = 0, width = 2560, height = 16 },
+    --position = Static { xpos = 0, ypos = 0, width = 2560, height = 16 },
+    position = Top,
     lowerOnStart = True,
     commands = [
-        Run Weather "EDDK" ["-t","<tempC>C <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+        Run Weather "EDDK" ["-t","<tempC>C <skyCondition>","-L","10","-H","20","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
         Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3> <total4> <total5> <total6> <total7>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
